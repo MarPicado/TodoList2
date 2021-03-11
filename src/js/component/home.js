4f890a27a17170295514551b6cc6385467c2e1dc
Margoth <mloreana@gmail.com>
2021-03-11 04:32:30 +0000

    to do

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,20 +5,50 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [task, setTask] = useState([]);
+
+	function addItem(e) {
+		if (e.key === "Enter" && e.target.value !== "") {
+			setTask(task.concat(e.target.value));
+			e.target.value = "";
+		}
+	}
+
+	function deleteItem(index) {
+		if (index > -1) {
+			const filterData = task.filter(item => item !== task[index]);
+			setTask(filterData);
+		}
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="title">My To Do List</h1>
+			<div className="inputValue">
+				<input
+					type="text"
+					name="item"
+					placeholder="Add the items here"
+					onKeyPress={e => addItem(e)}></input>
+				<div className="list">
+					<ul className="lineForm">
+						{task.map((value, index) => {
+							return (
+								<li
+									className="listForm"
+									onClick={() => {
+										deleteItem(index);
+									}}>
+									{value}{" "}
+									<span className="positionIcons">
+										<i className="fas fa-trash"></i>
+									</span>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 }
